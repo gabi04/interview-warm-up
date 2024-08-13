615f186fa16e21abf86fe89880df9a227586325f
Gabriela Suárez <gabrielasu@iadb.org>
2024-08-12 20:57:18 -0500

    questions page

--- src/app/pages/questions/questions.component.ts
+++ src/app/pages/questions/questions.component.ts
@@ -5,8 +5,26 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './questions.component.html',
-  styleUrl: './questions.component.scss'
+  styleUrl: './questions.component.scss',
 })
 export class QuestionsComponent {
+  // TODO types
+  questions = [
+    { id: 1, text: 'What can you tell me about yourself?' },
+    { id: 2, text: 'What are the Maine responsibilities in your current job?' },
+    { id: 3, text: 'What are your most relevant skills? ' },
+  ];
+  lastIndex = 0;
+  currentIndex = 0;
 
+  onLastClick() {
+    const lastIndexAux = this.lastIndex;
+    this.currentIndex = this.lastIndex;
+    this.lastIndex = lastIndexAux;
+  }
+
+  onNextClick() {
+    this.lastIndex = this.currentIndex;
+    this.currentIndex = Math.floor(Math.random() * this.questions.length);
+  }
 }
