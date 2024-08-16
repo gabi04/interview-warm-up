d7eb763f629f3e81edec23e3a9378d007f0990dc
Gabriela Suárez <gabrielasu@iadb.org>
2024-08-16 10:11:09 -0500

    add dist to gitigone again

--- dist/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-UNQKZZPW.mjs";import"./chunk-JUBPY5Q3.mjs";import"./chunk-QRYRJ4QU.mjs";import"./chunk-HUJSUSXZ.mjs";import"./chunk-RPFJGOYG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
