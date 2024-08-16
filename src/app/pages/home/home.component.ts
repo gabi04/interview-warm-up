e56b9fc10d0e256335636ab7d52946867b90d721
Gabriela Suárez <gabrielasu@iadb.org>
2024-08-15 20:18:12 -0500

    remove comment

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
-/*TODO
-  dado angular 17
-  que esto va a terner su propio modulo
-  la arquitectura que planee
-  ¿quiero que esto sea standalone?
-*/
 @Component({
   selector: 'app-home',
   standalone: true,
